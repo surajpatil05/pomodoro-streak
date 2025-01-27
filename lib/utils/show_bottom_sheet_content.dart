eec4697b0b875246bcd71b859831985008d5d6e9
Suraj Patil <surajpatil05@gmail.com>
2025-01-28 02:08:43 +0530

    added screenutil responsiveness to this widget

--- lib/utils/show_bottom_sheet_content.dart
+++ lib/utils/show_bottom_sheet_content.dart
@@ -1,8 +1,13 @@
 // custom_bottom_sheet.dart
 import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:pomodoro_streak/providers/break_timer_notifier.dart';
 import 'package:pomodoro_streak/providers/focus_timer_notifier.dart';
+
 import 'package:pomodoro_streak/providers/select_dropdown_notifier.dart';
 
 void showBottomSheetContent(BuildContext context, WidgetRef ref) {
@@ -15,24 +20,24 @@ void showBottomSheetContent(BuildContext context, WidgetRef ref) {
     context: context,
     backgroundColor: Colors.black,
     shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      borderRadius: BorderRadius.vertical(top: Radius.circular(16.r)),
     ),
     builder: (context) {
       return Container(
-        height: 300,
-        padding: EdgeInsets.all(16),
+        height: 300.h,
+        padding: EdgeInsets.all(16.r),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               'Select Option',
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 20.sp,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
               ),
             ),
-            SizedBox(height: 16),
+            SizedBox(height: 16.h),
             Expanded(
               child: ListView.builder(
                 itemCount: options.length,
@@ -60,19 +65,19 @@ void showBottomSheetContent(BuildContext context, WidgetRef ref) {
                       Navigator.pop(context);
                     },
                     child: Container(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-                      margin: EdgeInsets.symmetric(vertical: 8),
+                      padding: EdgeInsets.symmetric(
+                          vertical: 12.h, horizontal: 16.w),
+                      margin: EdgeInsets.symmetric(vertical: 8.h),
                       decoration: BoxDecoration(
                         color: isSelected
                             ? Colors.white.withValues(alpha: 0.2)
                             : Colors.transparent,
-                        borderRadius: BorderRadius.circular(8),
+                        borderRadius: BorderRadius.circular(8.r),
                       ),
                       child: Text(
                         option,
                         style: TextStyle(
-                          fontSize: 14,
+                          fontSize: 14.sp,
                           color: isSelected ? Colors.white : Colors.grey[400],
                           fontWeight:
                               isSelected ? FontWeight.bold : FontWeight.normal,
